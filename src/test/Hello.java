0d723f59078542d1c41b641414516cd09265039b
hwj <463367328@qq.com>
2018-07-22 18:07:46 +0800

    测试提交

---
+++ src/test/Hello.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Hello {
+
+    static {
+        System.out.print("测试提交");
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.print(1);
+        System.out.print("Hello World");
     }
 }
